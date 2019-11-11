fd23528068238ad2fd73466cd37dae3f9e3f2154
胡永强 <huyongqiang@star-net.cn>
2019-11-11 09:43:35 +0800

    消除windows linus兼容问题

--- src/pages/api-detail/ApiDetail.jsx
+++ src/pages/api-detail/ApiDetail.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import {connect} from 'dva';
+import React from "react";
+import { connect } from "dva";
 
-import ApiDetailUI from './ApiDetailUI'
+import ApiDetailUI from "./ApiDetailUI";
 
 // 业务组件
-const ApiDetail = (props) => {
-    const {clickedApi, lang} = props;
-    return (
-        <ApiDetailUI clickedApi={clickedApi}/>
-    );
-}
-
+const ApiDetail = props => {
+  const { clickedApi, lang } = props;
+  return <ApiDetailUI clickedApi={clickedApi} />;
+};
 
 // 复制全局state中的属性到组件的props中
 function mapStateToProps(state) {
-    return {clickedApi: state.DocInfoModel.clickedApi};
+  return { clickedApi: state.DocInfoModel.clickedApi };
 }
 
 export default connect(mapStateToProps)(ApiDetail);
